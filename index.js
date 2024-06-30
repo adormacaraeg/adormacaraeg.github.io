688fdf3b4162aca81176282f49fe7d361d3a8f49
adormacaraeg <adormacaraeg723@gmail.com>
2024-06-30 18:32:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 
 function submitcomment(){
- commentbox.textContent += comment.value.toString() + "\n" ;*/
+ commentbox.textContent += comment.value.toString() + "\n" ;
   comment.value= " ";
 }
 submit.addEventListener("click",submitcomment)
